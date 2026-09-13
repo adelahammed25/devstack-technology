11f29510bbf254b9408d1b62387b9de93d1ef151
adelahammed25 <adelahammed.25@gmail.com>
2026-09-13 23:43:22 +0600

    loadin fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Hero } from "./components/Hero";
 import { Footer } from "./components/Footer";
 
 const allTech = async (): Promise<Tptechnology[]> => {
-  const response = await fetch("/data.json");
+  const response = await fetch("/devstack-technology/data.json");
   const techData = await response.json();
   return techData;
 };
